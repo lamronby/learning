c7a8456d8e9fafc26ce3ec2802034c9fb1c17769
Chris <chrisj732@gmail.com>
2014-08-27 18:02:36 -0600

    Initial commit of ASP.NET MVC SportsStore learning project

---
+++ ASP.NET MVC/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository _repository;
+
+        public ProductController(IProductsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ ASP.NET MVC/SportsStore/SportsStore.WebUI/Infrastructure/AutofacControllerFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class AutofacControllerFactory : DefaultControllerFactory
+    {
+        private readonly IContainer _container;
+
+        public AutofacControllerFactory()
+        {
+            var builder = new ContainerBuilder();
+            AddBindings(builder);
+            _container = builder.Build();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return (controllerType == null) ? null : (IController) _container.Resolve(controllerType);
+        }
+
+        private void AddBindings(ContainerBuilder builder)
+        {
+            var mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surf board", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+            }.AsQueryable());
+
+            builder.RegisterInstance(mock.Object).As<IProductsRepository>();
+
+            // register controllers
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
